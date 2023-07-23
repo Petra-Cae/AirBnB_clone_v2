d75febb207944eb009bece4441a434a2a7e4c2c6
Petraoje <petraoje4real@gmail.com>
2023-07-23 03:55:56 +0000

    return a c and the value of text vars

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ Returns a greeting from 0.0.0.0:5000 """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Returns HBNB from 0.0.0.0:5000/hbnb """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """
+    Returns C followed by the value of the text var
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
